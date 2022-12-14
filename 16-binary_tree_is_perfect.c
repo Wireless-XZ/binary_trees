8889651e29e2ceb9812fe038ea72830a31c95b07
arabiu033 <arabiu033@gmail.com>
2022-12-14 13:20:44 -0800

    fix bug

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -7,14 +7,36 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int left, right;
+	int left, right, hleft, hright;
 
 	if (!tree)
 		return (0);
 
-	left = node_height(tree->left);
-	right = node_height(tree->right);
-	return (left - right == 0 ? 1 : 0);
+	left = check_full(tree->left);
+	right = check_full(tree->right);
+	hleft = node_height(tree->left);
+	hright = node_height(tree->right);
+	return ((left + right == 2) && (hleft == hright) ? 1 : 0);
+}
+
+/**
+ * check_full - a helpig function
+ * @tree: pointer to the tree node
+ * Return: number of nodes
+ */
+int check_full(const binary_tree_t *tree)
+{
+	int left, right;
+
+	if (!tree)
+		return (1);
+
+	left = check_full(tree->left);
+	right = check_full(tree->right);
+
+	if (tree && ((!tree->right && !tree->left) || (tree->left && tree->right)))
+		return (left && right ? 1 : 0);
+	return (0);
 }
 
 /**
