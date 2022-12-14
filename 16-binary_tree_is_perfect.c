475fd4880a128b88d079de8d3538106831ac319f
arabiu033 <arabiu033@gmail.com>
2022-12-14 12:42:29 -0800

    16. Is perfect

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - a function that measures the balance factor of a tree
+ * @tree:  a pointer to the root node of the tree to measure the balance factor
+ * Return: balance factor
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int left, right;
+
+	if (!tree)
+		return (0);
+
+	left = node_height(tree->left);
+	right = node_height(tree->right);
+	return (left - right == 0 ? 1 : 0);
+}
+
+/**
+ * node_height - a helpig function
+ * @tree: pointer to the tree node
+ * Return: number of nodes
+ */
+int node_height(const binary_tree_t *tree)
+{
+	int right, left;
+
+	if (!tree)
+		return (0);
+
+	left = node_height(tree->left);
+	right = node_height(tree->right);
+	return ((left > right ? left : right) + 1);
+}
